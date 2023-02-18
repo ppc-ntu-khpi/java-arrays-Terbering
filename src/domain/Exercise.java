259cd6fc4059360480fbf0782d1008da7e22f8dc
Terbering <113172992+Terbering@users.noreply.github.com>
2023-02-18 20:25:22 +0000

    p

--- src/domain/Exercise.java
+++ src/domain/Exercise.java
@@ -1,7 +1,60 @@
-package domain;
-
 public class Exercise {
-    public static float Calculate(int R){
-        return (float) (2*Math.PI*Math.sqrt(R));
+public static void main (String [] args){
+    //вхідний масив
+    int features[] = { 2, 2, 11, 11, 7, 6, 6, 8, 7, 7 };
+    //масив із повтореннями
+    int reps[] = new int[features.length/2];
+    
+    int temp = features[0];
+    //кількість повторень temp
+    int c = 0;
+    //макс кількість повторень у features
+    int maxCount = 0;
+    //index для запису в масив reps
+    int r = 0;
+
+    //записуємо масив
+    for(int i = 1; i < features.length + 1; i++) {
+        //зроблено для перевірки останнього ел, звідти та умова i < features.length + 1
+        int cur = features.length > i ? features[i] : -features[i-1];
+        if(cur == temp) {
+            c++;
+        } else {
+          if (maxCount < c) {
+              maxCount = c;
+              r = 0;
+              reps = new int[features.length/2];
+              reps[r] = temp;
+          } else if(maxCount == c) {
+              r++;
+              reps[r] = temp;
+          }
+          c = 0;
+        }
+        temp = cur;
+    }
+    //обрізаємо в новий масив
+    int result[] = new int[r+1];
+    for(int i = 0; i < r + 1; i++) {
+        result[i] = reps[i];
+    }
+    //виводимо сортований масив
+    for (int k : sort(result)) {
+        System.out.println(k);
     }
 }
+
+//виводимо сортований масив
+public static int[] sort (int[] array) {
+    for (int i = 0; i < array.length; i++) {
+        for (int j = 0; j < array.length; j++) {
+            if (array[i] > array[j]) {
+                int sTemp = array[i];
+                array[i] = array[j];
+                array[j] = sTemp;
+            }
+         }
+      }
+    }
+}
+    return array;
--- src/test/TestResult.java
+++ src/test/TestResult.java
@@ -1,12 +1,59 @@
-package test;
+public static void main (String [] args){
+    //вхідний масив
+    int features[] = { 2, 2, 11, 11, 7, 6, 6, 8, 7, 7 };
+    //масив із повтореннями
+    int reps[] = new int[features.length/2];
+    
+    int temp = features[0];
+    //кількість повторень temp
+    int c = 0;
+    //макс кількість повторень у features
+    int maxCount = 0;
+    //index для запису в масив reps
+    int r = 0;
 
-import domain.Exercise;
-
-public class TestResult {
-
-    public static void main(String[] args) {
-
-        int R=10;
-        System.out.println("Area of a circle for R="+R+"m is "+Exercise.Calculate(R)+"m"+'\u00B2');
+    //записуємо масив
+    for(int i = 1; i < features.length + 1; i++) {
+        //зроблено для перевірки останнього ел, звідти та умова i < features.length + 1
+        int cur = features.length > i ? features[i] : -features[i-1];
+        if(cur == temp) {
+            c++;
+        } else {
+          if (maxCount < c) {
+              maxCount = c;
+              r = 0;
+              reps = new int[features.length/2];
+              reps[r] = temp;
+          } else if(maxCount == c) {
+              r++;
+              reps[r] = temp;
+          }
+          c = 0;
+        }
+        temp = cur;
+    }
+    //обрізаємо в новий масив
+    int result[] = new int[r+1];
+    for(int i = 0; i < r + 1; i++) {
+        result[i] = reps[i];
+    }
+    //виводимо сортований масив
+    for (int k : sort(result)) {
+        System.out.println(k);
     }
 }
+
+//виводимо сортований масив
+public static int[] sort (int[] array) {
+    for (int i = 0; i < array.length; i++) {
+        for (int j = 0; j < array.length; j++) {
+            if (array[i] > array[j]) {
+                int sTemp = array[i];
+                array[i] = array[j];
+                array[j] = sTemp;
+            }
+        }
+    }
+}
+}
+    return array;
