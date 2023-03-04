b71f676586c5369543fd0ef32d2d18ce41eb4380
Terbering <113172992+Terbering@users.noreply.github.com>
2023-03-04 18:59:20 +0000

    Exercise

--- src/test/TestResult.java
+++ src/test/TestResult.java
@@ -1,59 +1,46 @@
-public static void main (String [] args){
-    //вхідний масив
-    int features[] = { 2, 2, 11, 11, 7, 6, 6, 8, 7, 7 };
-    //масив із повтореннями
-    int reps[] = new int[features.length/2];
-    
-    int temp = features[0];
-    //кількість повторень temp
-    int c = 0;
-    //макс кількість повторень у features
-    int maxCount = 0;
-    //index для запису в масив reps
-    int r = 0;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
-    //записуємо масив
-    for(int i = 1; i < features.length + 1; i++) {
-        //зроблено для перевірки останнього ел, звідти та умова i < features.length + 1
-        int cur = features.length > i ? features[i] : -features[i-1];
-        if(cur == temp) {
-            c++;
-        } else {
-          if (maxCount < c) {
-              maxCount = c;
-              r = 0;
-              reps = new int[features.length/2];
-              reps[r] = temp;
-          } else if(maxCount == c) {
-              r++;
-              reps[r] = temp;
-          }
-          c = 0;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the size of the array: ");
+        int size = scanner.nextInt();
+
+        // Створюємо масив інтів розміром size
+        int[] arr = new int[size];
+        System.out.println("Enter the elements of the array: ");
+        
+        // Заповнюємо масив arr з клавіатури
+        for (int i = 0; i < size; i++) {
+            arr[i] = scanner.nextInt();
         }
-        temp = cur;
-    }
-    //обрізаємо в новий масив
-    int result[] = new int[r+1];
-    for(int i = 0; i < r + 1; i++) {
-        result[i] = reps[i];
-    }
-    //виводимо сортований масив
-    for (int k : sort(result)) {
-        System.out.println(k);
-    }
-}
 
-//виводимо сортований масив
-public static int[] sort (int[] array) {
-    for (int i = 0; i < array.length; i++) {
-        for (int j = 0; j < array.length; j++) {
-            if (array[i] > array[j]) {
-                int sTemp = array[i];
-                array[i] = array[j];
-                array[j] = sTemp;
+        int maxCount = 0;
+        int mostFrequent = 0;
+        
+        // Створюємо HashMap для підрахунку кількості входжень кожного числа
+        Map<Integer, Integer> countMap = new HashMap<>();
+
+        // Додаємо кожне число з масиву до countMap, збільшуючи його лічильник
+        for (int num : arr) {
+            if (countMap.containsKey(num)) {
+                countMap.put(num, countMap.get(num) + 1);
+            } else {
+                countMap.put(num, 1);
             }
         }
+
+        // Проходимось по елементах countMap, знаходячи ключ з найбільшою кількістю входжень
+        for (Map.Entry<Integer, Integer> entry : countMap.entrySet()) {
+            if (entry.getValue() > maxCount) {
+                maxCount = entry.getValue();
+                mostFrequent = entry.getKey();
+            }
+        }
+
+        // Виводимо найчастіше зустрічаючеся число та кількість його входжень
+        System.out.println("Most frequent number is " + mostFrequent + " and it occurs " + maxCount + " times.");
     }
 }
-}
-    return array;
